0c522b1e089b12c278a4f859fc22eab7eff6bdf3
zhixuanziben <714919199@qq.com>
2017-05-10 15:56:55 +0800

    完成音乐播放页面基本的样式，但是色彩还有点问题

--- src/views/music/music.vue
+++ src/views/music/music.vue
@@ -1,35 +1,45 @@
 <template>
   <div class="music-audio-wrap">
-    <div>
-      <div>
-        {{musicName}}
-        <div v-for="(item, index) in artists">
+    <header>
+      <div class="header-iconback" @click="back">
+        <span class="icon-右键"></span>
+      </div>
+      <div class="header-title">
+        <span class="header-music-name">
+          {{musicName}}
+        </span>
+        <div v-for="(item, index) in artists" class="header-music-artists">
           {{item.name}}
           <span v-if="index < (artists.length - 1)">/</span>
         </div>
       </div>
-      <div @click="getLyric(id)">
-        <img :src="imgUrl" style="width: 200px; height: 200px" v-if="showImg">
+    </header>
+    <section class="music-main">
+      <img src="../../assets/imgs/stick_bg.png" class="music-main-stick">
+      <div class="music-main-pic" @click="getLyric(id)">
+        <img :src="imgUrl" v-if="showImg">
         <div v-else>{{lyric}}</div>
       </div>
-      <div>
-        <span></span>
-        <span @click="getComment">评论</span>
-        <span></span>
-      </div>
-      <div>
-        <span>{{currentMin}}:{{currentSec}}</span>
-        <mu-slider v-model="value1"></mu-slider>
-        <span>{{fullMin}}:{{fullSec}}</span>
-      </div>
-      <div class="">
-        <span class="icon-list"></span>
-        <span class="icon-prewmusic" @click="prew"></span>
-        <span class="icon-next" @click="next"></span>
-        <span class="icon-pause" @click="play" v-if="isPlaying"></span>
-        <span class="icon-play" @click="play" v-else></span>
-      </div>
-    </div>
+    </section>
+    <section class="music-section-icon">
+      <span class="icon-aixin"></span>
+      <span class="icon-comment" @click="getComment"></span>
+      <span class="icon-省略"></span>
+    </section>
+    <section class="music-section-time">
+      <span>{{currentMin}}:{{currentSec}}</span>
+      <mu-slider v-model="value1" class="slider"></mu-slider>
+      <span>{{fullMin}}:{{fullSec}}</span>
+    </section>
+    <section class="music-section-contro">
+      <span class="icon-列表循环"></span>
+      <span class="icon-prewmusic" @click="prew"></span>
+      <span class="icon-pause" @click="play" v-if="isPlaying"></span>
+      <span class="icon-play" @click="play" v-else></span>
+      <span class="icon-next" @click="next"></span>
+      <span class="icon-list"></span>
+    </section>   
+    <div class="music-bg" :style="{'background-image':'url(' + imgUrl + '?param=500y500' + ')'}"></div>
   </div>
 </template>
 
@@ -138,23 +148,130 @@
           }).then(() => {
             this.showImg = !this.showImg
           })
+      },
+      back () {
+        this.$router.go(-1)
       }
     }
   }
 </script>
 
 <style lang="scss">
+  @mixin element-center {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
   .music-audio-wrap {
-    background-color: gray;
+    .music-bg {
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      height: 100%;
+      width: 100%;
+      filter: blur(0.4rem);
+      background-repeat: no-repeat;
+      background-size: cover;
+      z-index: -1;
+    }
+    > header {
+      display: flex;
+      height: 1rem;
+      border-bottom: 1px solid #fff9e8;
+      .header-iconback {
+        width: 0.55rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #fff9e8;
+      }
+      .header-title {
+        flex: 1;
+        @include element-center;
+        .header-music-name {
+          font-size: 0.15rem;
+          color: #fff9e8;
+        }
+        .header-music-artists {
+          font-size: 0.12rem;
+          color: #665a3b;
+        }
+      }
+    }
+    .music-main {
+      .music-main-stick {
+        display: block;
+        height: 1.2rem;
+        margin: 0 auto;
+        margin-top: -0.05rem;
+        margin-bottom: -0.2rem;
+      }
+      .music-main-pic {
+        background: url(../../assets/imgs/cd_wrapper.png);
+        background-size: cover;
+        margin: 0 auto;
+        width: 4rem;
+        height: 4rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        animation: rotate 25s linear infinite;
+        // animation-play-state: paused;
+        img {
+          width: 66%;
+          margin: 0 auto;
+          border-radius: 50%;
+        }
+      }
+    }
+    .music-section-icon {
+      display: flex;
+      width: 80%;
+      margin: 0.7rem auto 0.4rem;
+      color: #fff;
+      font-size: 0.4rem;
+      > span {
+        flex: 1;
+        text-align: center;
+      }
+    }
+    .music-section-time {
+      display: flex;
+      width: 80%;
+      margin: 0 auto;
+      color: #fff;
+      .slider {
+        margin-left: 0.2rem;
+        margin-right: 0.2rem;
+      }
+    }
+    .music-section-contro {
+      display: flex;
+      color: #fff;
+      margin: 0.5rem 0;
+      height: 1rem;
+      line-height: 1rem;
+      > span {
+        flex: 1;
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 0.4rem;
+      }
+      .icon-play,
+      .icon-pause {
+        font-size: 0.7rem;
+      }
+    }
   }
-  .slider {
-    background-color: red;
-    height: 0.10rem;
-    width: 2rem;
-  }
-  .slider > div {
-    width: 0.15rem;
-    height: 0.15rem;
-    margin-left: 0.5rem; 
+  @keyframes rotate {
+    0%{ 
+      transform:rotate(0deg);
+    } 100%{
+      transform:rotate(360deg);
+    }
   }
 </style>
