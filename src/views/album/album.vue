b34b7666fd0d569e9824e0d3bc2a0321ff1bee1c
zhixuanziben <714919199@qq.com>
2017-05-20 23:07:33 +0800

    fix bug

--- src/views/album/album.vue
+++ src/views/album/album.vue
@@ -1,40 +1,42 @@
 <template>
   <div>
-    <list-title title="专辑"></list-title>
-    <div class="album-msg-wrap">
-      <div class="album-pic">
-        <img :src="album.picUrl">
-      </div>
-      <div class="album-msg">
-        <h3>{{album.name}}</h3>
-        <p>歌手：<span v-for="item in album.artists">{{item.name}}</span> ></p>
-        <p>发行时间：{{(new Date(album.publishTime)).getFullYear()}}.{{(new Date(album.publishTime)).getMonth()+1}}.{{(new Date(album.publishTime)).getDate()}}</p>
-      </div>
-    </div>
-    <div class="listData">
-      <div>
-        <div>
-          <span class="icon-collection"></span>
+    <div class="header-wrap">
+      <list-title title="专辑"></list-title>
+      <div class="album-msg-wrap">
+        <div class="album-pic">
+          <img :src="album.picUrl">
         </div>
-        {{info.likedCount}}
-      </div>
-      <div @click="goComment(data.id)">
-        <div>
-          <span class="icon-comment"></span>
+        <div class="album-msg">
+          <h3>{{album.name}}</h3>
+          <p>歌手：<span v-for="item in album.artists">{{item.name}}</span> ></p>
+          <p>发行时间：{{(new Date(album.publishTime)).getFullYear()}}.{{(new Date(album.publishTime)).getMonth()+1}}.{{(new Date(album.publishTime)).getDate()}}</p>
         </div>
-        {{info.commentCount}}
       </div>
-      <div>
+      <div class="listData">
         <div>
-          <span class="icon-share"></span>
+          <div>
+            <span class="icon-collection"></span>
+          </div>
+          {{info.likedCount}}
         </div>
-        {{info.shareCount}}
-      </div>
-      <div>
+        <div @click="goComment(data.id)">
+          <div>
+            <span class="icon-comment"></span>
+          </div>
+          {{info.commentCount}}
+        </div>
         <div>
-          <span class="icon-download"></span>
+          <div>
+            <span class="icon-share"></span>
+          </div>
+          {{info.shareCount}}
         </div>
-        下载
+        <div>
+          <div>
+            <span class="icon-download"></span>
+          </div>
+          下载
+        </div>
       </div>
     </div>
     <music-list :songs="data.songs"></music-list>
@@ -80,6 +82,10 @@
 </script>
 
 <style lang="scss" scoped>
+  .header-wrap {
+    background: #df2d2d;
+    color: #fff;
+  }
   .album-msg-wrap {
     display: flex;
     .album-pic {
