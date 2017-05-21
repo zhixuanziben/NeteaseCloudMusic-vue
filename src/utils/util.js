ed9dc4aba138193cc4e65bb31307a5c9c3709536
智轩 <zhixuanziben@gmail.com>
2017-05-21 22:43:54 +0800

    Update util.js

--- src/utils/util.js
+++ src/utils/util.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 function likeComment (id, cid, t, type) {
   axios.get(`http://localhost:3000/comment/like?id=${id}&cid=${cid}&t=${t}&type=${type}`).then((res) => {
-    console.log(res)
   })
 }
 
